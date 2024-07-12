f64d670bc2d6546ee1c20dbf7c137d00105a2f8c
Godnumbersoon <6611850014@mut.ac.th>
2024-07-12 17:22:23 +0700

    wasdad0

--- login.php
+++ login.php
@@ -1,10 +1,9 @@
 <?php
 echo "<h1><table> </h1>";
 echo "<h1> User ee: </h1>";
+echo "<h1> User ee: </h1>";
+echo "<h1> User adadadee: </h1>";
+echo "<h1> User eeadsadad: </h1>";
 echo"<h1>Password : </h1>";
 echo "<h1></table> </h1>";
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> d93fe8a29cec898ae8a9118158769daa01a9f47b
